3c5aec120a7519261937bb0a4431c4bf9affaf2a
ToeOoWaiYan <toeoowaiyan4252@gmail.com>
2025-11-27 23:19:54 +0630

    remove edit

--- resources/js/Pages/Shipment/Edit.vue
+++ resources/js/Pages/Shipment/Edit.vue
@@ -2,31 +2,21 @@
 // Layout
 import AdminLayout from '@/Layouts/AdminLayout.vue';
 // Components
-import TextInput from '@/Components/Input/TextInput.vue';
-import Label from '@/Components/Label/Label.vue';
 import ToggleInput from '@/Components/Input/ToggleInput.vue';
 import Button from '@/Components/Button/Button.vue';
 //Libs
-import { route } from 'ziggy-js';
 import { useForm } from '@inertiajs/vue3';
 
-const props = defineProps({
-    'letterType': {
-        type: Object,
-        required: true,
-        default: () => ({})
-    }
-})
+const props = defineProps({})
 // Ref
 const form = useForm({
     _method: 'PUT',
-    name: props.letterType.data.name,
-    status: true
+
 })
 
 // Handlers
 const submit = () => {
-    form.put(route('letter-types.update', { letter_type: props.letterType.data.id }))
+
 }
 
 const cancel = () => {
@@ -40,17 +30,11 @@ const cancel = () => {
         <template #content>
             <div class="px-4 py-2 bg-gray-600">
                 <div class="flex items-center justify-between">
-                    <h2 class="text-xl font-bold">Letter Types</h2>
+                    <h2 class="text-xl font-bold"></h2>
                 </div>
             </div>
 
             <form @submit.prevent="submit" class="p-8 flex flex-col w-1/2 gap-6">
-                <div class="flex flex-col gap-2">
-                    <Label for="name" mandatory>Letter Type</Label>
-                    <TextInput id="name" placeholder="Input Name" v-model="form.name" />
-                </div>
-
-                <ToggleInput v-model="form.status" label="Status" />
 
                 <div class="flex justify-end gap-4">
                     <Button type="button" background="bg-gray-500 text-white"
